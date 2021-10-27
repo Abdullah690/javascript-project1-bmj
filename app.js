ea199eb9fadba360ef2ae3e77a0e04f3bbb696cd
Abdullah690 <as250543@gmail.com>
2021-10-27 16:16:54 +0500

    javascript project

---
+++ app.js
@@ -0,0 +1,70 @@
+
+                                ////// JavaScript//////
+// javaScript ek scripting language hn. Aur (Interpretation language hn jo humhri codding ko
+// line by line read krti hn aur left to right read krtha hn but Interpretation thora slow 
+// read krtha hn.(Campilation javaScript ke code ko ek sath read krta hn aur fast read krtha hn)./////
+
+
+                          ////  DATA TYPES  ////
+
+                           /// String ///
+                           /// Number ///
+                           /// Boolean ///
+                           /// Undefine ///
+                           /// NuN    ///
+
+        ////MATHS EXPRESSITION////
+
+                /// + ///
+                /// - ///
+                /// * ///
+                /// / ///
+                /// % ///
+
+//  var x = 17;
+//  var y = x++ - ++x + ++x + --x + ++x - ++x + ++x;
+//      //  17  - 19  +  20 +  19 +  20 - 21  + 22 //
+//  alert(y);
+
+
+
+                                
+// var userName = (5);
+// var age1 = (3);
+// var age = (88);
+// var add = ( userName* age -age1);
+// alert(add);
+
+
+
+
+
+     //////  MARK SHEET   //////
+
+var name = prompt("Enter your name");
+var age  = prompt("Enter your age");
+var eng  = prompt ("Eng Marks");
+var urdu = prompt ("Urdu Marks");
+var isl  = prompt ("Islamiat Marks");
+var math = prompt ("Maths Marks");
+var sci  = prompt ("Science Marks");
+var obtained = prompt ("Obtained Marks");
+var total = prompt ("Total Marks");
+var percentage = ("Percentage : " +" "+ obtained/total * 100 );
+alert(percentage);
+
+
+document.write("Name :" +" "+ name);
+document.write(  " </br> Age :" +" "+ age);
+document.write(  " </br> English Marks :" +" "+ eng);
+document.write("</br> Urdu Marks : "+" "+ urdu)
+document.write("</br> Islamiat Marks : "+" "+isl)
+document.write("</br> Maths Marks : "+" "+math)
+document.write("</br> Science Marks : "+" "+sci)
+document.write("</br> Obtained Marks : "+" "+obtained)
+document.write("</br> Total Marks : "+" "+total)
+document.write("</br> Percentage : "+" "+percentage)
+
+
+
+
